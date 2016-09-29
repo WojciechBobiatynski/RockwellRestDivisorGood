849025db8d7b2728213e25e2bbe6064a8add9386
Jakub.BENTYN <Jakub.BENTYN@mycompany.com>
2016-09-29 12:38:12 +0000

    Poprawka pobierania loginu użytkownika w LoginUtils
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@109 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-model/src/main/java/pl/sodexo/it/gryf/common/utils/LoginUtils.java
+++ gryf-model/src/main/java/pl/sodexo/it/gryf/common/utils/LoginUtils.java
@@ -2,6 +2,7 @@ package pl.sodexo.it.gryf.common.utils;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 /**
  * Created by Tomasz.Bilski on 2015-06-10.
@@ -11,9 +12,10 @@ public abstract class LoginUtils {
     public static String getLogin(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication != null){
-            Object principal = authentication.getPrincipal();
+            //TODO zamienic na GryfUUser po rozdzieleniu commons i modelu
+            User principal = (User) authentication.getPrincipal();
             if(principal != null){
-                return principal.toString();
+                return principal.getUsername();
             }
         }
         return null;
