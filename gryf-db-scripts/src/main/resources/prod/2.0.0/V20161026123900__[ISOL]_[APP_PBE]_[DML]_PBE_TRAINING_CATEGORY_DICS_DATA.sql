e387648acad98ad513a31fa573bec8b6ba22b0de
Jakub.BENTYN <Jakub.BENTYN@mycompany.com>
2016-10-26 11:09:02 +0000

    #80 Dodanie rekordów tabeli słownikowej z kategoriami szkoleń
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@205 a1b3532b-92c3-dd42-b132-913ad673e08d

---
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20161026123900__[ISOL]_[APP_PBE]_[DML]_PBE_TRAINING_CATEGORY_DICS_DATA.sql
@@ -0,0 +1,3 @@
+INSERT INTO APP_PBE.PBE_TRAINING_CATEGORY_DICS VALUES ('JAZ', 'Szkolenie jêzykowe', 0, 'system', SYSDATE, 'system', SYSDATE);
+INSERT INTO APP_PBE.PBE_TRAINING_CATEGORY_DICS VALUES ('KCY', 'Szkolenie z zakresu kompetencji cyfrowych', 1, 'system', SYSDATE, 'system', SYSDATE);
+INSERT INTO APP_PBE.PBE_TRAINING_CATEGORY_DICS VALUES ('INN', 'Inne certyfikowane szkolenie', 2, 'system', SYSDATE, 'system', SYSDATE);
