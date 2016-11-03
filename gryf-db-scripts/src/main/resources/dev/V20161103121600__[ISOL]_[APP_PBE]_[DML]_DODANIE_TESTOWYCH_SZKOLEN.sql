937fde6805d5e417e621d6a55b0f2b82562c25cd
Jakub.BENTYN <Jakub.BENTYN@mycompany.com>
2016-11-03 11:39:18 +0000

    #81 Dodanie przykładowych szkoleń dla środowiska DEV
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@246 a1b3532b-92c3-dd42-b132-913ad673e08d

---
+++ gryf-db-scripts/src/main/resources/dev/V20161103121600__[ISOL]_[APP_PBE]_[DML]_DODANIE_TESTOWYCH_SZKOLEN.sql
@@ -0,0 +1,8 @@
+INSERT INTO APP_PBE.TI_TRAININGS VALUES(EAGLE.TI_TRA_SEQ.nextval, 1080, 'Szkolenie z zakresu pakietu Office', 3000,
+  to_date('2016-12-25', 'yyyy-MM-dd'), to_date('2017-01-05', 'yyyy-MM-dd'), 'Warszawa', 30, 50, 'KCY', 'system', SYSDATE, 'system', SYSDATE);
+
+INSERT INTO APP_PBE.TI_TRAININGS VALUES(EAGLE.TI_TRA_SEQ.nextval, 1104, 'Kurs jêzyka angielskiego - Poziom C1', 2500,
+  to_date('2016-09-11', 'yyyy-MM-dd'), to_date('2016-09-13', 'yyyy-MM-dd'), 'Gdynia', 30, 50, 'JAZ', 'system', SYSDATE, 'system', SYSDATE);
+
+INSERT INTO APP_PBE.TI_TRAININGS VALUES(EAGLE.TI_TRA_SEQ.nextval, 1004, 'Szkolenie kandydatów na egzaminatorów WORD', 8000,
+  to_date('2016-05-10', 'yyyy-MM-dd'), to_date('2016-08-10', 'yyyy-MM-dd'), 'Kraków', 30, 50, 'INN', 'system', SYSDATE, 'system', SYSDATE);
