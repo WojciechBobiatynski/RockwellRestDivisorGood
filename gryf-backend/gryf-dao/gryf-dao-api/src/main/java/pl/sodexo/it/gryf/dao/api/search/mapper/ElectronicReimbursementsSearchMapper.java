baddf1df560e10bda86e45a00babe16499c76cad
Adam.KMIECINSKI <Adam.KMIECINSKI@mycompany.com>
2016-11-14 10:37:04 +0000

    #101 SG018 Rozliczenie bonow: Rozliczenie - dodanie statusuów do e-rozliczeń
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@305 a1b3532b-92c3-dd42-b132-913ad673e08d

---
+++ gryf-backend/gryf-dao/gryf-dao-api/src/main/java/pl/sodexo/it/gryf/dao/api/search/mapper/ElectronicReimbursementsSearchMapper.java
@@ -0,0 +1,18 @@
+package pl.sodexo.it.gryf.dao.api.search.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import pl.sodexo.it.gryf.common.criteria.electronicreimbursements.ElctRmbsCriteria;
+import pl.sodexo.it.gryf.common.dto.publicbenefits.electronicreimbursements.ElctRmbsDto;
+
+import java.util.List;
+
+/**
+ * Mapper do operacji na e-rozliczeniach
+ *
+ * Created by akmiecinski on 14.11.2016.
+ */
+public interface ElectronicReimbursementsSearchMapper {
+
+    List<ElctRmbsDto> findEcltRmbsListByCriteria(@Param("criteria") ElctRmbsCriteria criteria);
+
+}
---
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20161114112600__[ISOL]_[APP_PBE]_[DML]_E_RMBS_STATUSES_INSERT.sql
@@ -0,0 +1,4 @@
+Insert into APP_PBE.E_REIMBURSEMENT_STATUSES (ID,NAME,ORDINAL) values ('T_CRR','Do korekty',null);
+Insert into APP_PBE.E_REIMBURSEMENT_STATUSES (ID,NAME,ORDINAL) values ('T_RMS','Do rozliczenia',null);
+Insert into APP_PBE.E_REIMBURSEMENT_STATUSES (ID,NAME,ORDINAL) values ('T_VRF','Do weryfikacji',null);
+Insert into APP_PBE.E_REIMBURSEMENT_STATUSES (ID,NAME,ORDINAL) values ('REIMB','Rozliczone',null);
