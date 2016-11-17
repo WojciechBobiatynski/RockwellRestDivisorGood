70fd7b79fcb43e411083f8c120db8382f31cb387
Krzysztof.ANTCZAK <Krzysztof.ANTCZAK@mycompany.com>
2016-11-17 16:41:39 +0000

    #119 [TI] Refactor frontendu i backendu - wydzielenie training instance (DAO, service, mapper, controller, angular service)
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@381 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-gui/gryf-web-ti/src/main/webapp/js/canceltrainingreservation/gryf-cancelTrainingReservation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-angular.module("gryf.ti").config(["$stateProvider", function($stateProvider) {
-    $stateProvider.state("cancelReservation", {
-        url: "/cancelReservation",
-        templateUrl: contextPath + "/templates/canceltrainingreservation/cancelTrainingReservation.html",
-        controller: "CancelTrainingReservationController"
-    });
-}]);
