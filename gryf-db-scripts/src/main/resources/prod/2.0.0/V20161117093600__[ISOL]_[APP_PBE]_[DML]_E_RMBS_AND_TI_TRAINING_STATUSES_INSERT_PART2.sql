98c6453e07d5a51ac5ec97a21e2b5373c48957a7
Adam.KMIECINSKI <Adam.KMIECINSKI@mycompany.com>
2016-11-17 09:05:29 +0000

    #101 SG018 Rozliczenie bonow: Rozliczenie - koljne statusy do instancji szkolenia i rozliczeń
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@365 a1b3532b-92c3-dd42-b132-913ad673e08d

---
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20161117093600__[ISOL]_[APP_PBE]_[DML]_E_RMBS_AND_TI_TRAINING_STATUSES_INSERT_PART2.sql
@@ -0,0 +1,3 @@
+Insert into APP_PBE.E_REIMBURSEMENT_STATUSES (ID,NAME,ORDINAL) values ('NEW','Nowe',null);
+
+insert into APP_PBE.TI_TRAINING_INSTANCE_STATUSES (ID, NAME, ORDINAL) values ('T_RMS', 'Do rozliczenia', 4);
