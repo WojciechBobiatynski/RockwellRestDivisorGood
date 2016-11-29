f26705a06bfcf142718574ac02b2f31720ec0963
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2016-11-29 10:01:31 +0000

    usuniecie pól z order flow anulowanie
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@499 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/others/sciezki_do_repozytorium_plikow.sql
+++ gryf-db-scripts/src/main/resources/others/sciezki_do_repozytorium_plikow.sql
@@ -1,3 +1,10 @@
 update EAGLE.ADM_PARAMETERS
 set value = 'C:\Users\Isolution\Desktop\GryfWorkspace\GryfFileRepo\'
 WHERE NAME = 'GRYF_PATH_ATTACHMENTS';
+
+
+
+update APP_PBE.ORDER_FLOW_STATUS_TRANSITIONS
+set AUTOMATIC = '0'
+where ACTION_ID in (10045, 10034, 10023);
+
