2009ae959d20403c9cf919c3e8137f2aced829cc
Krzysztof.ANTCZAK <Krzysztof.ANTCZAK@mycompany.com>
2017-01-17 10:08:20 +0000

    Błąd #214 - [TI] W przypadku gdy użytkownik nie ma umów pokazuj jego dane, ale nie informuj o braku umowy podczas rezerwacji
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@738 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-gui/gryf-web-ti/src/main/webapp/js/app/trainingreservation/modal/reservationModalControllers.js
+++ gryf-gui/gryf-web-ti/src/main/webapp/js/app/trainingreservation/modal/reservationModalControllers.js
@@ -1,5 +1,6 @@
-angular.module("gryf.ti").controller("ReservationModalController", ["$scope", "$stateParams", "TrainingSearchService", "TrainingReservationService", "UserService",
-    function($scope, $stateParams, TrainingSearchService, TrainingReservationService, UserService) {
+angular.module("gryf.ti").controller("ReservationModalController", ["$scope", "$stateParams", "GryfModals",
+    "TrainingSearchService", "TrainingReservationService", "UserService",
+    function($scope, $stateParams, GryfModals, TrainingSearchService, TrainingReservationService, UserService) {
     $scope.close = $scope.$close;
     $scope.patternNumberOnly = /^\d+$/;
     $scope.training = {data: null};
@@ -19,9 +20,17 @@ angular.module("gryf.ti").controller("ReservationModalController", ["$scope", "$
         trainingReservationDto.trainingId = $scope.training.data.trainingId;
         trainingReservationDto.individualId = $scope.userTrainingReservationData.data.id;
         trainingReservationDto.verificationCode = $scope.individualUser.data.verificationCode;
-        trainingReservationDto.contractId = $scope.userTrainingReservationData.data.contracts[0].id;
+        if($scope.userTrainingReservationData.data.contracts[0]) {
+            trainingReservationDto.contractId = $scope.userTrainingReservationData.data.contracts[0].id;
+        }
         trainingReservationDto.toReservedNum = $scope.toReservedNum;
         trainingReservationDto.version = $scope.training.data.version;
+
+        if(!trainingReservationDto.contractId) {
+            GryfModals.openModal(GryfModals.MODALS_URL.ERROR_INFO,
+                {message: "Nie można zarezerwować szkolenia, ponieważ ta osoba nie posiada żadnej umowy."});
+            return;
+        }
 
         TrainingReservationService.reserveTraining(trainingReservationDto).then(function() {
             $scope.close(true);
