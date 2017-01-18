5b4331960f9cb1ed1460cdcde28092171be4c35a
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2017-01-18 11:34:44 +0000

    zmainy w parametrach aplikacji
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@760 a1b3532b-92c3-dd42-b132-913ad673e08d

---
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20170118103000__[ISOL]_[APP_PBE]_[DML]_ADM_PARAMETERS.sql
@@ -0,0 +1,13 @@
+MERGE INTO EAGLE.ADM_PARAMETERS ug USING (
+  SELECT
+    'GRYF_PATH_ACCOUNTING_DOCUMENT' NAME,
+    'accounting_documents/dev/' VALUE,
+    'Podfolder do zapisu plików/raportów/za³¹czników dla e-rozliczeñ w ramach WUP (Gryf)' DESCRIPTION
+  FROM dual
+) ins
+ON (ug.NAME = ins.NAME)
+WHEN MATCHED THEN
+  UPDATE SET ug.VALUE = ins.VALUE, ug.DESCRIPTION = ins.DESCRIPTION
+WHEN NOT MATCHED THEN INSERT
+  (NAME,VALUE,DESCRIPTION)
+  VALUES (ins.NAME, ins.VALUE, ins.DESCRIPTION);
