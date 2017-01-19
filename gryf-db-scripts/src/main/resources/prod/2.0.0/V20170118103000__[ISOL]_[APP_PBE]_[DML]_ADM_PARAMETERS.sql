83c862a508ef6be3056748a592a2666ae3daf113
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2017-01-19 08:35:25 +0000

    poprawki parametrów
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@770 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/prod/2.0.0/V20170118103000__[ISOL]_[APP_PBE]_[DML]_ADM_PARAMETERS.sql
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20170118103000__[ISOL]_[APP_PBE]_[DML]_ADM_PARAMETERS.sql
@@ -12,3 +12,21 @@ WHEN NOT MATCHED THEN INSERT
   (NAME,VALUE,DESCRIPTION)
   VALUES (ins.NAME, ins.VALUE, ins.DESCRIPTION);
 
+
+
+MERGE INTO EAGLE.ADM_PARAMETERS ug USING (
+                                           SELECT
+                                             'GRYF_CDN_URL' NAME,
+                                             '//cdn.sodexo.pl/gryf/' VALUE,
+                                             'Url do CDN' DESCRIPTION
+                                           FROM dual
+                                         ) ins
+ON (ug.NAME = ins.NAME)
+WHEN MATCHED THEN
+UPDATE SET ug.VALUE = ins.VALUE, ug.DESCRIPTION = ins.DESCRIPTION
+WHEN NOT MATCHED THEN INSERT
+  (NAME,VALUE,DESCRIPTION)
+VALUES (ins.NAME, ins.VALUE, ins.DESCRIPTION);
+
+
+
--- gryf-db-scripts/src/main/resources/prod/2.0.0/V20170118160300__[ISOL]_[APP_PBE]_[DML]_ADM_PARAMETERS_CHANGE_CDN_URL.sql
+++
@@ -1,3 +0,0 @@
-UPDATE EAGLE.ADM_PARAMETERS
-  SET VALUE = '//cdn.sodexo.pl/gryf'
-WHERE name = 'GRYF_CDN_URL'
