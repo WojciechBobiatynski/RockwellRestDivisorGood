bb26746be91b699e432f78530b38df37e89a9348
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2017-01-19 08:25:16 +0000

    poprawa wysyłki maila resetujacego hasło - poprawny url
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@769 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/others/przydatne_sql_na_dev.sql
+++ gryf-db-scripts/src/main/resources/others/przydatne_sql_na_dev.sql
@@ -18,6 +18,36 @@ from APP_FIN.INVOICE_LINES
 where INV_ID = 814 and (POS_NUM <> 1 or POS_TYPE <> 'S');
 
 
+MERGE INTO EAGLE.ADM_PARAMETERS ug USING (
+                                           SELECT
+                                             'GRYF_TI_USER_URL' NAME,
+                                             'http://localhost:8080/gryf-ti-fo/' VALUE,
+                                             'URL aplikacji TI' DESCRIPTION
+                                           FROM dual
+                                         ) ins
+ON (ug.NAME = ins.NAME)
+WHEN MATCHED THEN
+UPDATE SET ug.VALUE = ins.VALUE, ug.DESCRIPTION = ins.DESCRIPTION
+WHEN NOT MATCHED THEN INSERT
+  (NAME,VALUE,DESCRIPTION)
+VALUES (ins.NAME, ins.VALUE, ins.DESCRIPTION);
+
+
+
+MERGE INTO EAGLE.ADM_PARAMETERS ug USING (
+                                           SELECT
+                                             'GRYF_IND_USER_URL' NAME,
+                                             'http://localhost:8080/gryf-ind-fo/' VALUE,
+                                             'URL aplikacji IND' DESCRIPTION
+                                           FROM dual
+                                         ) ins
+ON (ug.NAME = ins.NAME)
+WHEN MATCHED THEN
+UPDATE SET ug.VALUE = ins.VALUE, ug.DESCRIPTION = ins.DESCRIPTION
+WHEN NOT MATCHED THEN INSERT
+  (NAME,VALUE,DESCRIPTION)
+VALUES (ins.NAME, ins.VALUE, ins.DESCRIPTION);
+
 update APP_PBE.TI_TRAININGS
 set ACTIVE = 'Y',
   DEACTIVATE_USER = null,
