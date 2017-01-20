07c7b4c0e72701191e3b0c72379f9773c0f5f55d
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2017-01-20 10:47:06 +0000

    zmiana w mailach
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@793 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/prod/2.0.0/V20170113155400__[ISOL]_[APP_PBE]_[DML]_SZABLONY_MAILI.sql
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20170113155400__[ISOL]_[APP_PBE]_[DML]_SZABLONY_MAILI.sql
@@ -118,7 +118,7 @@ THEN INSERT (ID, EMAIL_BODY_TEMPLATE, EMAIL_SUBJECT_TEMPLATE, DESCRIPTION) VALUE
 MERGE INTO APP_PBE.EMAIL_TEMPLATES msg USING (SELECT 'E_CONF_GRA' ID ,'Sz. P. {$firstName} {$lastName},
 Uprzejmie informujemy, ¿e rozliczono bony w ramach projektu {$grantProgramName} dotycz¹ce szkolenia {$trainingName}.
 
-W za³¹czniku znajdzie Pan/Pani potwierdzenie rozliczenia us³ugi, tj. dokument pn. Potwierdzenie realizacji dofinansowania.
+W za³¹czniku znajdzie Pan/Pani potwierdzenie rozliczenia us³ugi, tj. dokument pn. Potwierdzenie realizacji dofinansowania oraz potwierdzemoe zap³aty nale¿noœci za us³ugê.
 
 Z powa¿aniem
 Zespó³ ds. obs³ugi klienta
@@ -131,7 +131,7 @@ Sodexo Benefits and Rewards Services Polska
 ul. K³obucka 25; 02-699 Warszawa
 Sodexo. World Leader in Quality of Life Services.' EMAIL_BODY_TEMPLATE,
 
-                                                     '{$grantProgramName} – potwierdzenie rozliczenia us³ugi' EMAIL_SUBJECT_TEMPLATE,
+                                                     '{$grantProgramName} – potwierdzenie rozliczenia us³ugi oraz potwierdzenie zap³aty nale¿noœci z tytu³u tej us³ugi' EMAIL_SUBJECT_TEMPLATE,
                                                      'Email do Uczestnika (MŒP) z potwierdzeniem rozliczenia us³ugi - systemowy/automatyczny' DESCRIPTION
                                               from dual) ins ON (msg.ID = ins.ID)
 WHEN MATCHED THEN
@@ -471,12 +471,124 @@ THEN INSERT (ID, EMAIL_BODY_TEMPLATE, EMAIL_SUBJECT_TEMPLATE, DESCRIPTION) VALUE
 
 
 
+/*
+
+update APP_PBE.EMAIL_TEMPLATES
+set EMAIL_TYPE = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Kierunek Kariera - Wojewódzki Urz¹d Pracy Kraków</title>
+
+<style type="text/css">
+  #outlook a {padding:0;}
+  p, span, strong, big, a {font-family: Arial, sans-serif; font-size: 300; }
+  p {margin: 19px 0; padding: 0;font-size: 13px; line-height: 19px;}
+  table td {border-collapse: collapse;}
+  table {border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+  img {display: block; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;}
+  a img {border: none;}
+  a, a:link, a:visited {text-decoration: none; color: #00788a}
+  a:hover {text-decoration: underline;}
+  h2,h2 a,h2 a:visited,h3,h3 a,h3 a:visited,h4,h5,h6,.t_cht {color:#000 !important}
+  .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td {line-height: 100%}
+  .ExternalClass {width: 100%;}
+</style>
+<!--[if gte mso 9]>
+  <style>
+  /* Target Outlook 2007 and 2010 */
+  </style>
+<![endif]-->
+</head>
+<body style="width:100%; margin:0; padding:0; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" style="margin:0; padding:0; width:100%; line-height: 100% !important;">
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640">
+        <tr>
+          <td valign="top"><img src="http://cdn.sodexo.pl/gryf/mailing/header.gif" width="640" height="121" title="Ma³opolska, Urz¹d Pracy, Instytucja Województwa Ma³opolskiego Wojewódzki Urz¹d Pracy w Krakowie" style="margin:0;padding:0;border:0;"></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" height="64" style="background-color:#304f87;text-align: center;vertical-align:middle;"><span style="font-size: 13px; line-height: 50px;color:#ffffff; text-transform: uppercase;font-weight: 700;">KIERUNEK KARIERA</span></td>
+  </tr>
+  <tr>
+    <td valign="top" style="height: 29px;background: #ffffff;"><br></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #ffffff; color:#696668; font-size: 13px; line-height: 19px; padding:0 29px;">
+			  <p style="font-size: 13px; line-height: 19px;">{$emailPlainBodyTemplates}</p>
+		  </td>
+		</tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="height: 29px;background: #ffffff;"><br></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #304f87; color:#ffffff; font-size: 13px; line-height: 19px; padding: 0 29px; text-align: center;"><p style="font-size: 13px; line-height: 19px;">Wszystkie informacje o projekcie pn. "Kierunek Kariera" dostêpne s¹ na stronie:<br><a style="color:#ffc720;font-size: 13px; line-height: 19px;" href="http://www.pociagdokariery.pl" title="www.pociagdokariery.pl">www.pociagdokariery.pl</a></p><p style="font-size: 13px; line-height: 19px;"><a style="color:#ffc720;font-size: 13px; line-height: 19px;" href="http://www.facebook.com/pociagdokariery" title="www.facebook.com/pociagdokariery">www.facebook.com/pociagdokariery</a></p></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top"><img src="http://cdn.sodexo.pl/gryf/mailing/footer.gif" width="640" height="100" title="Fundusze Europejskie Program Regionalny, Ma³polska, Unia Europejska Europejski Fundusz Socjalny" style="margin:0;padding:0;border:0;"></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #f7f7f7; color:#696668; font-size: 13px; line-height: 19px; padding:0 29px; text-align: center;"><p style="font-size: 13px; line-height: 19px;">E-mail zosta³ wygenerowany automatycznie, prosimy na niego nie odpowiadaæ.</p></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #ffffff; color:#696668; font-size: 11px; line-height: 15px; padding:0 29px; text-align: center;"><p style="font-size: 11px; line-height: 15px;">Wiadomoœæ zosta³a wys³ana przez Sodexo Benefits and Rewards Services Polska Sp. z o.o., ul. K³obucka 25, 02-699 Warszawa S¹d Rejonowy dla m. st. Warszawy w Warszawie, XIII Wydzia³ Gospodarczy Krajowego Rejestru S¹dowego, Kapita³ Zak³adowy 3,000,000 PLN, KRS: 0000033826, NIP: 522-23-57-343, poniewa¿ Twój adres e-mail znajduje siê na liœcie kontaktów.</p></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #f7f7f7; color:#696668; font-size: 13px; line-height: 19px; padding:29px 29px 0 29px; text-align: center;"><a href="http://www.sodexo.pl/" title="Sodexo Benefits and Rewards Services Polska Sp. z o.o." style="margin: 0 auto;"><img width="110" height="36" src="http://cdn.sodexo.pl/gryf/mailing/sodexo.gif" title="Sodexo Benefits and Rewards Services Polska Sp. z o.o." alt="Sodexo Benefits and Rewards Services Polska Sp. z o.o."></a></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td valign="top" style="background: #ffffff;"><table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="background: #ffffff;">
+        <tr>
+          <td valign="top" style="background-color: #f7f7f7; color:#696668; font-size: 13px; line-height: 19px; padding:0 29px; text-align: center;"><p style="font-size: 13px; line-height: 19px;">&copy; 2017 <a style="color:#696668;font-size: 13px; line-height: 19px;" href="http://www.sodexo.pl/" title="Sodexo Benefits and Rewards Services Polska Sp. z o.o.">Sodexo Benefits and Rewards Services Polska Sp. z o.o.</a></p></td>
+        </tr>
+      </table></td>
+  </tr>
+</table></body></html>
+'
+where ID in (
+  'KK_ORDER',
+  'E_REIMB',
+  'CNF_PYMT',
+  'E_CONF_GRA',
+  'CORR_NOTIF',
+  'PIN_SEND',
+  'PIN_RESEND',
+  'RESET_LINK',
+  'EXPI_PROD',
+  'EXPI_REIMB',
+  'TI_ACCESS',
+  'RET_REIMB',
+  'VC_SEND');
 
 
 
 
-
-
+*/
 
 
 
