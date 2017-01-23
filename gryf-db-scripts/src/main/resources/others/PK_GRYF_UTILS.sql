295c0098546197b9db104c0ad5b20100473efc60
Mariusz.POLAK <Mariusz.POLAK@mycompany.com>
2017-01-23 11:15:32 +0000

    17/17 zmiany instalowane na WARE, TEST i PROD w weekend 2017-01-19-2017-01-22
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@820 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/others/PK_GRYF_UTILS.sql
+++ gryf-db-scripts/src/main/resources/others/PK_GRYF_UTILS.sql
@@ -1,5 +1,5 @@
-create or replace PACKAGE       PK_GRF_UTILS AS
-  /******************************************************************************
+CREATE OR REPLACE PACKAGE EAGLE.PK_GRF_UTILS AS
+/******************************************************************************
      NAME:       PK_GRF_UTILS
      PURPOSE:    PLSQL Utils for GRYF WEBAPP
 
@@ -8,8 +8,9 @@ create or replace PACKAGE       PK_GRF_UTILS AS
      ---------  ----------  ---------------  ------------------------------------
      -1.0       2015-09-01  MGU              Cratered
      -0.9       2016-12-07  MAPO             Cratered deeper. WUP debit note creation
+   -0.8       2016-12-30  MAPO             WUP Credit Note
 
-  ******************************************************************************/
+******************************************************************************/
   FUNCTION Amount_Verbally(a_amount NUMBER) RETURN VARCHAR2;
 
   FUNCTION Next_Business_Day(a_date DATE) RETURN DATE;
@@ -25,18 +26,13 @@ create or replace PACKAGE       PK_GRF_UTILS AS
   FUNCTION Get_Nth_Day(a_date DATE, a_nth_day NUMBER, a_day_type VARCHAR2) RETURN DATE;
 
 
-  -- procedura robocza do testów noty obci¹¿eniowo-ksiêgowej dla Uczestnika (beneficjenta programu)
-  --    PROCEDURE Create_Debit_Note_Worker( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, a_order_id IN NUMBER );
-  -- procedura robocza, do testów wew. noty uznaniowej
-  PROCEDURE Create_Credit_Note_Worker( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, a_ermb_id IN NUMBER, a_type in VARCHAR2 default 'CUS' );
-
-
   -- utwórz notê obci¹¿eniowo-ksiêgow¹ dla Uczestnika (beneficjenta)
-  PROCEDURE Create_Pb_Cus_Note( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, o_87_invoice_number OUT VARCHAR2, a_order_id IN NUMBER, a_sign_user IN VARCHAR2 DEFAULT 'PONICHTERA BART£OMIEJ' );
+    PROCEDURE Create_Pb_Cus_Note( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, o_87_invoice_number OUT VARCHAR2, a_order_id IN NUMBER, a_sign_user IN VARCHAR2 DEFAULT USER);
 
   -- utwórz notê uznaniow¹ dla uczestnika
-  PROCEDURE Create_Pb_Rmb_Note( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, a_ermb_id IN NUMBER, a_sign_user IN VARCHAR2 DEFAULT 'PONICHTERA BART£OMIEJ', a_type in VARCHAR2 default 'CUS' );
+    PROCEDURE Create_Pb_Rmb_Note( o_inv_id OUT NUMBER, o_invoice_number OUT VARCHAR2, o_invoice_type OUT VARCHAR2, o_invoice_date OUT DATE, a_ermb_id IN NUMBER, a_sign_user IN VARCHAR2 DEFAULT '', a_type in VARCHAR2 default 'CUS' );
 
 
 
 END PK_GRF_UTILS;
+/
