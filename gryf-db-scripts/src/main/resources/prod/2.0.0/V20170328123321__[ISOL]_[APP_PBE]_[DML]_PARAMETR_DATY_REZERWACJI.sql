d0ba244559a2e9392178c62f6917a0cd291ef84f
Tomasz.BILSKI <Tomasz.BILSKI@mycompany.com>
2017-03-29 21:09:59 +0000

    Zmiana #310 Dodanie parametru sterującego do kiedy Uczestnik może rejestrować się na Szkolenie (instancję): realizacja
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@951 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-db-scripts/src/main/resources/prod/2.0.0/V20170328123321__[ISOL]_[APP_PBE]_[DML]_PARAMETR_DATY_REZERWACJI.sql
+++ gryf-db-scripts/src/main/resources/prod/2.0.0/V20170328123321__[ISOL]_[APP_PBE]_[DML]_PARAMETR_DATY_REZERWACJI.sql
@@ -1,7 +1,7 @@
 
 Insert into APP_PBE.GRANT_PROGRAM_PARAM_TYPES (ID, NAME, DESCRIPRTION) values ('ENROLL_GAP','Dni do rezewacji szkolenia',
-                                                                               '= 0 – mogê rezerwowaæ przed rozpoczêciem oraz w dniu szkolenia
-                                                                               < 0 – mogê rezerwowaæ przed rozpoczêciem i po rozpoczêciu szkolenia np. dla wartoœci -2 a¿ do 2ch dni po rozpoczêciu
-                                                                               > 0 – mogê rezerwowaæ wy³¹cznie przed rozpoczêciem szkolenia np. dla wartoœci 3 do 3ch dni przed rozpoczêciem');
+'= 0 – mogê rezerwowaæ przed rozpoczêciem oraz w dniu szkolenia
+< 0 – mogê rezerwowaæ przed rozpoczêciem i po rozpoczêciu szkolenia np. dla wartoœci -2 a¿ do 2ch dni po rozpoczêciu
+> 0 – mogê rezerwowaæ wy³¹cznie przed rozpoczêciem szkolenia np. dla wartoœci 3 do 3ch dni przed rozpoczêciem');
 
 Insert into APP_PBE.GRANT_PROGRAM_PARAMS (ID, GRANT_PROGRAM_ID, PARAM_ID, VALUE, DATE_FROM, DATE_TO) values (12, 100, 'ENROLL_GAP', '0', null, null);
