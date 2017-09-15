a6f231041fcf49f8f954fb7e8fa172963677d39e
Mariusz.POLAK <Mariusz.POLAK@mycompany.com>
2017-09-15 14:47:25 +0000

    #378 Niemożliwe wykonanie Odrzucenia Rozliczenia
    git-svn-id: http://svn.sodexo.com.pl/svn/gryf/trunk@1002 a1b3532b-92c3-dd42-b132-913ad673e08d

--- gryf-gui/gryf-web-fo/src/main/webapp/js/publicbenefits/electronicreimbursements/electronicReimbursementsControllers.js
+++ gryf-gui/gryf-web-fo/src/main/webapp/js/publicbenefits/electronicreimbursements/electronicReimbursementsControllers.js
@@ -125,7 +125,7 @@ angular.module('gryf.electronicreimbursements').controller("announce.electronicR
         };
 
         $scope.rejectButtonVisible = function(){
-            return $scope.eReimbObject.entity != null && $scope.eReimbObject.entity.statusCode === 'T_RMS' && eReimbObject.entity.terminated;
+            return $scope.eReimbObject.entity != null && $scope.eReimbObject.entity.statusCode === 'T_RMS' && $scope.eReimbObject.entity.terminated;
         };
 
         $scope.reimburseButtonVisible = function(){
