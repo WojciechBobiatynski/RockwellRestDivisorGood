22055065c17a3ee3ce9b765abebed7fe972ea6c7
Tomasz.Burnicki <tomasz.burnicki@gerrit.sodexo.com.pl>
2018-11-05 17:26:27 +0100

    t585 błąd przy probie rezerwacji szkolenia - dodanie uprawnien TYLKO select
    http://redmineerp.sodexo.com.pl/issues/585
    
    Change-Id: I7c691b6cfbb784bbbb449b51fb4a403bc210f3dc

--- gryf-db-scripts/src/main/resources/prod/2.2.0/V20181105175500__[SDX]_[APP_PBE]_[DDL]_GRANT_PROGRAM_PRODUCTS_ADD_PRIVILEGES.sql
+++ gryf-db-scripts/src/main/resources/prod/2.2.0/V20181105175500__[SDX]_[APP_PBE]_[DDL]_GRANT_PROGRAM_PRODUCTS_ADD_PRIVILEGES.sql
@@ -1,5 +1,5 @@
 -- uprawnienia
 BEGIN
-	dbms_output.put_line(SYS.PK_ADMIN.F_GRANT_TO_EAGLE(UPPER('${gryf.schema}'), 'SRV_EE_TI', 'GRANT_PROGRAM_PRODUCTS', 'ALL'));
+	dbms_output.put_line(SYS.PK_ADMIN.F_GRANT_TO_EAGLE(UPPER('${gryf.schema}'), 'SRV_EE_TI', 'GRANT_PROGRAM_PRODUCTS', 'SELECT'));
 END;
 /
