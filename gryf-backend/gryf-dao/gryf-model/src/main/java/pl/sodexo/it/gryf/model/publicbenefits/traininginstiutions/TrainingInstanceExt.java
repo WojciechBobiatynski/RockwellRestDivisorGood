07c2e1bd94763c2ec75408086628d75c1ad87d0f
Tomasz.Burnicki <tomasz.burnicki@gerrit.sodexo.com.pl>
2018-11-23 15:44:11 +0100

    t607 Import szkolen - poprawka zapytań
    http://redmineerp.sodexo.com.pl/issues/607
    
    Change-Id: Icf4fbf56396e44694ce2fe644270cd76a300c3d8

--- gryf-backend/gryf-dao/gryf-model/src/main/java/pl/sodexo/it/gryf/model/publicbenefits/traininginstiutions/TrainingInstanceExt.java
+++ gryf-backend/gryf-dao/gryf-model/src/main/java/pl/sodexo/it/gryf/model/publicbenefits/traininginstiutions/TrainingInstanceExt.java
@@ -24,8 +24,8 @@ import static pl.sodexo.it.gryf.model.publicbenefits.traininginstiutions.Trainin
 @NamedQueries({
         @NamedQuery(name = QUERY_TRAINING_INSTANCE_EXT_DELETE_ALL_TRAINING_INSTANCE_EXT, query = "delete from TrainingInstanceExt t " +
                 " where t.importJobId < :importJobId " +
-                " and t.importJobId in (select aj.id from AsynchronizeJob  aj JOIN aj.type jt " +
-                "                        where  jt.grantProgramId = :grantProgramId )"),
+                " and t.importJobId in (select aj.id from AsynchronizeJob  aj  " +
+                "                        where  aj.type.grantProgramId = :grantProgramId )"),
         @NamedQuery(name = "TrainingInstanceExt.findByIndOrderExternalId", query = "select count(t) from TrainingInstanceExt t where t.indOrderExternalId like CONCAT('%',:externalOrderId,'%') "),
 })
 @ToString
