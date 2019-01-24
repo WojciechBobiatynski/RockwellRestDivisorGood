81848cc06fd155d0f6bf95bebb4ef16407dac00c
KRAWCZYŃSKI Krzysztof <Krzysztof.KRAWCZYNSKI@sodexo.com>
2019-01-24 10:09:53 +0100

    t644 Dodanie odrębnej sekwencji dla APP_PBE.TI_TRAINING_INSTANCES_EXT.ID - Uprawnienia
    http://redmineerp.sodexo.com.pl/issues/644
    
    Poprawka nadawania uprawnień do sekwencji
    
    Change-Id: I34fd73ab8eadd3f441f4b32c74b6395f7ed9f294

--- gryf-db-scripts/src/main/resources/prod/2.5.1/V20190116095600__[SDX]_[APP_PBE]_[DDL]_TI_TRAININGS_IINSTANCES_EXT_ID_SEQ.sql
+++ gryf-db-scripts/src/main/resources/prod/2.5.1/V20190116095600__[SDX]_[APP_PBE]_[DDL]_TI_TRAININGS_IINSTANCES_EXT_ID_SEQ.sql
@@ -1,4 +1,3 @@
-
 CREATE SEQUENCE ${eagle.schema}.TI_TRA_INS_EXT_SEQ
   START WITH 1100000
   MAXVALUE 9999999999999999999999999999
@@ -10,6 +9,7 @@ CREATE SEQUENCE ${eagle.schema}.TI_TRA_INS_EXT_SEQ
 declare
   v_x varchar2(100);
 begin
-   v_x := SYS.PK_ADMIN.F_GRANT_TO_EAGLE('${eagle.schema}', 'SRV_EE', 'TI_TRA_INS_EXT_SEQ', 'SELECT');
+   v_x := SYS.PK_ADMIN.F_GRANT_TO_EAGLE(upper('${eagle.schema}'), 'SRV_EE', 'TI_TRA_INS_EXT_SEQ', 'SELECT');
    dbms_output.put_line(v_x);
 end;
+
